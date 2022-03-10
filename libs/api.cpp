d25083be729cc2a0dbbaa406784b21adca82597d
icedman <m4rvin2005@gmail.com>
2022-03-10 10:08:24 +0800

    add linux build

---
+++ libs/api.cpp
@@ -0,0 +1,22 @@
+#ifdef WIN64
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT extern "C" __attribute__((visibility("default"))) __attribute__((used))
+#endif
+
+#include <cstring>
+
+static char buffer[1024];
+
+EXPORT
+int add(int a, int b)
+{
+    return a + b;
+}
+
+EXPORT
+char* capitalize(char *str) {
+    strcpy(buffer, str);
+    buffer[0] = 'X';
+    return buffer;
+}
